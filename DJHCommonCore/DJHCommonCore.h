288c375f3c8c6ffe0f099b4889dc960ee4b3bb2e
duanjiahuan <jhduan@estt.com.cn>
2019-07-16 14:40:50 +0800

    新建公共库模块工程

---
+++ DJHCommonCore/DJHCommonCore.h
@@ -0,0 +1,19 @@
+//
+//  DJHCommonCore.h
+//  DJHCommonCore
+//
+//  Created by duanjiahuan on 2019/7/16.
+//  Copyright © 2019 duanjiahuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DJHCommonCore.
+FOUNDATION_EXPORT double DJHCommonCoreVersionNumber;
+
+//! Project version string for DJHCommonCore.
+FOUNDATION_EXPORT const unsigned char DJHCommonCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DJHCommonCore/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+workspace 'DJHCommonCore'#workspace名称
+
+#公共库模块名称
+target 'DJHCommonCore' do
+
+platform :ios, '8.0'
+use_frameworks!
+project 'DJHCommonCore.xcodeproj'
+
+pod 'AFNetworking'
+
+end
